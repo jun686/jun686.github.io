57d71f5b68943f7ee9dd7a5a899c56f9a793fa7a
jun686 <2739936850@qq.com>
2024-03-03 13:17:42 +0800

    Site updated: 2024-03-03 13:17:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=[];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2019/04/21/hello-world/","2023/06/01/anzhiyu主题标签/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
