f2bbf801751c7c82afb26f5da58c2dd8b55b8877
jun686 <2739936850@qq.com>
2024-10-04 17:36:31 +0800

    Site updated: 2024-10-04 17:36:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/06/01/anzhiyu主题标签/","2019/04/21/hello-world/"];function toRandomPost(){
+var posts=["2023/06/01/anzhiyu主题标签/","2019/04/21/hello-world/","2024/10/04/接收NOAA19卫星APT信号/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
