c79313202a84dbea824f5a89e04d1a44a24594da
jun686 <2739936850@qq.com>
2024-11-03 11:16:39 +0800

    Site updated: 2024-11-03 11:16:38

--- server/idm/idm.ps1
+++ server/idm/idm.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://jun.now.cc/server/idm/idm.cmd'
+$DownloadURL = 'https://jun.now.cc/server/idm/IAS.cmd'
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
