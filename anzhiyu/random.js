2408981bed742d568e1c1025d416ddd258650f43
jun686 <2739936850@qq.com>
2025-07-24 17:45:22 +0800

    Site updated: 2025-07-24 17:45:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/06/01/anzhiyu主题标签/","2019/04/21/hello-world/","2024/10/04/接收NOAA19卫星APT信号/"];function toRandomPost(){
+var posts=["2024/10/04/接收NOAA19卫星APT信号/","2019/04/21/hello-world/","2023/06/01/anzhiyu主题标签/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
