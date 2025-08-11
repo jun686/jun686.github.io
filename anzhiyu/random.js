07a87470414272110b4f92f8d63111f92b5af82e
jun686 <2739936850@qq.com>
2025-08-11 17:00:06 +0800

    Site updated: 2025-08-11 17:00:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/04/接收NOAA19卫星APT信号/","2019/04/21/hello-world/","2023/06/01/anzhiyu主题标签/"];function toRandomPost(){
+var posts=["2024/10/04/接收NOAA19卫星APT信号/","2019/04/21/hello-world/","2023/06/01/anzhiyu主题标签/","2025/08/11/Tim上岛直播/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
